86458b72c4a9c893f6803ce8b45ff9bf64024868
dgran <dgranh@gmail.com>
2021-04-23 14:46:27 +0200

    testing server

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,22 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('eSoccer') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                {{-- <x-jet-welcome /> --}}
+                <div class="p-4">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel harum tenetur deleniti excepturi, eius quibusdam repudiandae provident. Explicabo nemo inventore velit excepturi, consequatur laudantium deleniti vitae earum odio, temporibus aperiam!</p>
+                    <p>Possimus placeat culpa doloremque soluta eum modi, id quasi, ratione incidunt quia quidem eos ut, eveniet non harum qui corrupti maiores facere quos sint at, iure quibusdam laboriosam. Commodi, aspernatur.</p>
+                    <p>Fugit facilis minima excepturi doloribus rerum fuga expedita molestias quidem suscipit sequi? Aperiam eligendi exercitationem accusamus. Optio natus nostrum vero repellendus. Qui tenetur repudiandae aperiam est. Quos necessitatibus qui voluptatibus.</p>
+                    <p>Quam vel perferendis, dolorum ducimus, hic maiores ab minus laudantium atque id veniam asperiores magni at non ipsa incidunt error expedita perspiciatis, velit quasi voluptas optio? Dignissimos eligendi animi incidunt!</p>
+                    <p>Quas voluptate excepturi sapiente in iusto ipsum vero debitis quod officiis quos veniam autem architecto nulla rerum quam laborum sequi, voluptas, corrupti maxime aperiam exercitationem a fugit nesciunt eligendi? Expedita!</p>
+
+                </div>
             </div>
         </div>
     </div>
